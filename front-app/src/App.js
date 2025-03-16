78230603316eb19f35111d621a1718d88de42280
SimBenoit <simbenoit325@gmail.com>
2025-03-16 16:40:45 -0400

    Many borpa things, cleaned up a bit

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Button, View } from 'react';
+
     import './App.css';
+    const [items, setItems] = ButtonClicked([]);
 
-    function App() {
-      const [items, setItems] = useState([]);
-
-      useEffect(() => {
-        fetch('http://localhost:5000/api/items')
+    const  ButtonClicked = () =>{
+      fetch('http://localhost:5000/api/items')
           .then((response) => response.json())
           .then((data) => setItems(data));
-      }, []);
+    }
+
+    function App() {
 
       return (
         <div className="App">
@@ -17,14 +18,10 @@ import React, { useEffect, useState } from 'react';
                 src="Borpa.png" 
                 alt="Rotating"
                 className="App-logo"
-            />
-            <h1>This is the list of items wowie</h1>
-            <ul>
-              {items.map((item) => (
-                <li key={item.id}>{item.name}</li>
-              ))}
-            </ul>
+            /> ceci n'est pas un borpa
+            <h1>We exist in the context right now</h1>
           </header>
+          <button className="App-title" onClick={() => ButtonClicked()}>There is nothing to worry about</button>
         </div>
       );
     }
