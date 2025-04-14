417ca444c2459c1a7be209d2f62e95980c2490d8
Ayrmin <90473964+SimBenoit@users.noreply.github.com>
2025-04-14 19:13:26 -0400

    Update entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Replace env vars in the config and start NGINX
-envsubst '$LAMBDA_URL $LAMBDA_SECRET' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '$LAMBDA_URL $LAMBDA_SECRET' < /etc/nginx/templates/nginx.conf > /etc/nginx/nginx.conf
 exec nginx -g "daemon off;"
