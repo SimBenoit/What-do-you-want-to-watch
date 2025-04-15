2d9b1eacd29074f5ad50f509a3142a80e823f198
Ayrmin <90473964+SimBenoit@users.noreply.github.com>
2025-04-15 10:21:42 -0400

    Update entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 # Optional debug: show env
 echo "LAMBDA_SECRET: $LAMBDA_SECRET"
 echo "LAMBDA_URL: $LAMBDA_URL"
+echo "LAMBDA_HOST: $LAMBDA_HOST"
 
 # Perform safe substitution
-envsubst '$LAMBDA_URL $LAMBDA_SECRET' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$LAMBDA_URL $LAMBDA_SECRET $LAMBDA_HOST' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 # Optional debug: show output config
 echo "---- Final rendered NGINX config ----"
