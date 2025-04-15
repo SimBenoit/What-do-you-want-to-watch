0d7a776375e6902c1759af4fad90bdeb57dd0bb3
Ayrmin <90473964+SimBenoit@users.noreply.github.com>
2025-04-15 15:20:57 -0400

    Bugfixing email (#11)
    * Update deploy-build.yml
    
    * Update variables.tf
    
    * Update Dockerfile.prod
    
    * Update main.tf
    
    * Update deploy-build.yml
    
    * Update Dockerfile.prod
    
    * Update Dockerfile.prod
    
    * Update Dockerfile.prod
    
    * Update entrypoint.sh
    
    * Update Dockerfile.prod
    
    * Rename nginx.conf to nginx.conf.template
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update nginx.conf.template
    
    * Update nginx.conf.template
    
    * Update nginx.conf.template
    
    * Update entrypoint.sh
    
    * Update entrypoint.sh
    
    * Update nginx.conf.template
    
    * Update nginx.conf.template
    
    * Update nginx.conf.template

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-# Replace env vars in the config and start NGINX
-envsubst '$LAMBDA_URL $LAMBDA_SECRET' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
-exec nginx -g "daemon off;"
+set -e
+
+# Optional debug: show env
+echo "LAMBDA_SECRET: $LAMBDA_SECRET"
+echo "LAMBDA_URL: $LAMBDA_URL"
+
+# Perform safe substitution
+envsubst '$LAMBDA_URL $LAMBDA_SECRET' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/conf.d/default.conf
+
+# Optional debug: show output config
+echo "---- Final rendered NGINX config ----"
+cat /etc/nginx/conf.d/default.conf
+echo "-------------------------------------"
+
+# Start NGINX
+exec nginx -g 'daemon off;'
